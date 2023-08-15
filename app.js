f4949ef7eaf22cb2370090352e03d0c21edccec8
Ashmit Singh <58002155+ashmitsinghk@users.noreply.github.com>
2023-08-15 12:40:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
-(function ($) {
-    $(document).ready(function(){
-      
-      $("#navbar").hide();
-      
-      $(function () {
-          $(window).scroll(function () {
-              // set distance user needs to scroll before we fadeIn navbar
-              if ($(this).scrollTop() > 500) {
-                  $('#navbar').fadeIn();
-              } else {
-                  $('#navbar').fadeOut();
-              }
-          });
-  
-      
-      });
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
 
-      $(".top").hide();
-      
-      $(function () {
-          $(window).scroll(function () {
-              // set distance user needs to scroll before we fadeIn navbar
-              if ($(this).scrollTop() > 500) {
-                  $('.top').fadeIn();
-              } else {
-                  $('.top').fadeOut();
-              }
-          });
-  
-      
-      });
-  
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function () {
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function () {
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
   });
-    }(jQuery));
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
+
+let scrollpos = window.scrollY
+const skills = document.querySelector(".skills-container")
+const skills_height = skills.offsetHeight
+
+const stick_skills = () => skills.classList.add("stick")
+const unstick_skills = () => skills.classList.remove("stick")
+
+const projects = document.querySelector(".projects-container")
+const projects_height = 141 * window.screen.height / 100
+
+const stick_projects = () => projects.classList.add("stick")
+const unstick_projects = () => projects.classList.remove("stick")
+
+const contact = document.querySelector(".contact-container")
+const contact_height = 218 * window.screen.height / 100
+
+const stick_contact = () => contact.classList.add("stick")
+const unstick_contact = () => contact.classList.remove("stick")
+
+window.addEventListener('scroll', function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= skills_height) { stick_skills() }
+  else { unstick_skills() }
+
+  if (scrollpos >= projects_height) { stick_projects() }
+  else { unstick_projects() }
+
+  if (scrollpos >= contact_height) { stick_contact() }
+  else { unstick_contact() }
+
+})
